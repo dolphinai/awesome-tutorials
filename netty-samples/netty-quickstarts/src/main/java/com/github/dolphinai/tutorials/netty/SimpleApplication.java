bb5704e1b9a2c854dc8e2e7feacb12de3c60ea47
beeai <franktang@live.com>
2019-12-29 06:50:39 +0800

    Initial commit

---
+++ netty-samples/netty-quickstarts/src/main/java/com/github/dolphinai/tutorials/netty/SimpleApplication.java
@@ -0,0 +1,13 @@
+package com.github.dolphinai.tutorials.netty;
+
+/**
+ * Hello world!
+ *
+ */
+public class SimpleApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ netty-samples/netty-quickstarts/src/test/java/com/github/dolphinai/tutorials/netty/SimpleApplicationTest.java
@@ -0,0 +1,20 @@
+package com.github.dolphinai.tutorials.netty;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple SimpleApplication.
+ */
+public class SimpleApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
